31e942c128a7f9223d7dda083dff10157f25ad67
MacBookPro <macbookpro@MacBookPro.local>
2018-11-08 12:28:30 +0100

    first connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
+const http = require ('http');
 const express = require ('express');
+const socketIO = require('socket.io');
 const path = require ('path');
+
 
 const publicPath = path.join (__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// socket is the individual socket as opposed
+io.on('connection', (socket) => {
+
+ console.log('New User Connected');
+
+ socket.on('disconnect', ()=> {
+     console.log('client disconnected connection');
+ });
+});
+
+
+server.listen(port, () => {
     console.log(`server is up ${port}`);
 });
 
